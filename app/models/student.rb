8bd85bdfec631780d9277bcd3a1e712a803d8dd4
lieby weintraub <liebahinda@gmail.com>
2019-07-25 05:56:26 +0000

    Automatically backed up by Learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -14,7 +14,7 @@ class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
  
-  def self.search(query)
+ def self.search(query)
     if query.present?
       where('NAME like ?', "%#{query}%")
     else
@@ -22,4 +22,3 @@ class Student < ActiveRecord::Base
     end
   end
 end
-
